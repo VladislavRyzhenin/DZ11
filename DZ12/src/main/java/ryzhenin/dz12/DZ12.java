9fcd86e62ce3b011179ec9981a97986b4339fa6d
VladislavRyzhenin <vip.ryzhenin228@mail.ru>
2026-06-01 12:13:26 +0300

    Add files via upload

---
+++ DZ12/src/main/java/ryzhenin/dz12/DZ12.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ryzhenin.dz12;
+
+/**
+ *
+ * @author vryzhenin
+ */
+public class DZ12 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
